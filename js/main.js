2fd18957ed43e07b928f849ac2ec1c5b8d5f10e2
Dmitrii <you@example.com>
2021-04-01 18:01:30 +0700

    section top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-$(function () {
+document.addEventListener("DOMContentLoaded", () => {
+   const menuBtn = document.querySelector('.menu__btn');
+   const rightsideMenu = document.querySelector('.rightside-menu');
+   const rightsideMenuClose = document.querySelector('.rightside-menu__close');
 
-})
+   menuBtn.addEventListener('click', function () {
+      rightsideMenu.classList.add('open');
+   });
+
+   rightsideMenuClose.addEventListener('click', () => {
+      rightsideMenu.classList.remove('open');
+   });
+});
+$(function () {
+   $('.top__slider').slick({
+      arrows: false,
+      dots: true,
+      fade: true,
+      autoplay: true,
+
+   });
+});
